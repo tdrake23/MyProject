923d920d52f970fc02a7f7d1e96b384e5f201708
TD <tomaszpdrake@gmail.com>
2017-02-25 17:01:48 -0400

    added cropping function

--- SobelAndCo/SobelAndCo.py
+++ SobelAndCo/SobelAndCo.py
@@ -2,6 +2,9 @@ from scipy import misc
 import matplotlib.pyplot as plt
 
 f = misc.face()
+
+
+f = f[300:600,300:600]
 misc.imsave('face.png', f) # uses the Image module (PIL)
 plt.imshow(f)
 plt.show()
